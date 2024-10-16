dbabd2a4da5636f93bdcd797122f8181f3d6fa16
Igor <igorberaldomo@gmai.com>
2024-10-16 17:10:26 -0300

    adicionado para card simples

--- backend.py
+++ backend.py
@@ -7,6 +7,9 @@ engine = sqlalchemy.create_engine(DATABASE_URL, pool_size=5, max_overflow=10)
 def select_names(nome):
     search_string = f"SELECT * FROM suvinil WHERE nome = '{nome}' or pantone_name = '{nome}' "
     resultset  = pd.read_sql(search_string, engine)
+    if resultset.empty:
+        search_string = f"SELECT * FROM suvinil where nome like ':nome' or pantone_name like ':nome' "
+        resultset  = pd.read_sql(search_string, engine, params={'nome': nome})
     print(resultset)
     return resultset
 
--- frontend.py
+++ frontend.py
@@ -85,35 +85,47 @@ def receivesuvinil():
     cores_df = pd.DataFrame(data).filter(['id','nome','red','green','blue','hexadecimal','pantone_código','pantone_name','pantone_hex','fornecedores']) 
     container = st.container()
     
-    # seleciona o numero do index
-    posição = pickcard()
-    card = (int(posição))
-    if (card +1) +3 > len(data):
-        card = len(data) -3
-        update(card)
-    if card <0:
-        card = 0
-        update(card)
+    if data is not None:
+        # seleciona o numero do index
+        posição = pickcard()
+        card = (int(posição))
+        if (card +1) +3 > len(data):
+            card = len(data) -3
+            update(card)
+        if card <0:
+            card = 0
+            update(card)
+
         
-    # pega os dados em variáveis
-    hexadecimal,fornecedores = (data[card]['hexadecimal']), data[card]['fornecedores']
-    nome,pantone_codigo = data[card]['nome'],data[card] ['pantone_código']
-    red,green,blue = data[card]['red'],data[card]['green'],data[card]['blue']
-    c,y,m,k = rgb_to_cmyk(data[card]['red'],data[card]['green'],data[card]['blue'])
-    hexadecimal1,fornecedores1 = (data[card+1]['hexadecimal']), data[card+1]['fornecedores']
-    nome1,pantone_codigo1 = data[card+1]['nome'],data[card+1] ['pantone_código']
-    red1,green1,blue1 = data[card+1]['red'],data[card+1]['green'],data[card+1]['blue']
-    c1,y1,m1,k1 = rgb_to_cmyk(data[card+1]['red'],data[card+1]['green'],data[card+1]['blue'])
-    hexadecimal2,fornecedores2 = (data[card+2]['hexadecimal']), data[card+2]['fornecedores']
-    nome2,pantone_codigo2 = data[card+2]['nome'],data[card+2]['pantone_código']
-    red2,green2,blue2 = data[card+2]['red'],data[card+2]['green'],data[card+2]['blue']
-    c2,y2,m2,k2 = rgb_to_cmyk(data[card+2]['red'],data[card+2]['green'],data[card+2]['blue'])
- 
-    with container:
-        st.button('anterior', key='anterior', on_click=subtract)
-        script = ("<div style='display: flex; flex-direction: row; justify-content: space-around; margin: 0px; padding:0px;width: 700px ;margin: 0px auto; height: 450px;'><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'><div id='container' style='background-color: {}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p><p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f} <br>magenta: {:.2f} <br>key:{:.2f} </p> </div><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'> <div id='container' style='background-color: {}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p> <p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f} <br>magenta: {:.2f} <br>key:{:.2f} </p> </div><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'> <div id='container' style='background-color: {}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p> <p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f}<br>magenta: {:.2f}<br>key:{:.2f} </p> </div></div>").format(hexadecimal,fornecedores,nome,pantone_codigo,red,green,blue,float(c),float(m),float(y),float(k),hexadecimal1,fornecedores1,nome1,pantone_codigo1,red1,green1,blue1,float(c1),float(m1),float(y1),float(k1),hexadecimal2,fornecedores2,nome2,pantone_codigo2,red2,green2,blue2,float(c2),float(m2),float(y2),float(k2))
-        st.markdown(script, unsafe_allow_html=True)
-        st.button('proximo', key='proximo', on_click=add)
+        if len(data) >= 0 and len(data) <=1:
+            hexadecimal,fornecedores = (data[card]['hexadecimal']), data[card]['fornecedores']
+            nome,pantone_codigo = data[card]['nome'],data[card] ['pantone_código']
+            red,green,blue = data[card]['red'],data[card]['green'],data[card]['blue']
+            c,y,m,k = rgb_to_cmyk(data[card]['red'],data[card]['green'],data[card]['blue'])
+            with container:
+                script = ("<div style='display: flex; flex-direction: row; justify-content: space-around; margin: 0px; padding:0px;width: 700px ;margin: 0px auto; height: 450px;'><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'><div id='container' style='background-color:{}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p><p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f} <br>magenta: {:.2f} <br>key:{:.2f} </p> </div></div>").format(hexadecimal,fornecedores,nome,pantone_codigo,red,green,blue,float(c),float(m),float(y),float(k))
+                st.markdown(script, unsafe_allow_html=True)
+
+        else:
+            # pega os dados em variáveis
+            hexadecimal,fornecedores = (data[card]['hexadecimal']), data[card]['fornecedores']
+            nome,pantone_codigo = data[card]['nome'],data[card] ['pantone_código']
+            red,green,blue = data[card]['red'],data[card]['green'],data[card]['blue']
+            c,y,m,k = rgb_to_cmyk(data[card]['red'],data[card]['green'],data[card]['blue'])
+            hexadecimal1,fornecedores1 = (data[card+1]['hexadecimal']), data[card+1]['fornecedores']
+            nome1,pantone_codigo1 = data[card+1]['nome'],data[card+1] ['pantone_código']
+            red1,green1,blue1 = data[card+1]['red'],data[card+1]['green'],data[card+1]['blue']
+            c1,y1,m1,k1 = rgb_to_cmyk(data[card+1]['red'],data[card+1]['green'],data[card+1]['blue'])
+            hexadecimal2,fornecedores2 = (data[card+2]['hexadecimal']), data[card+2]['fornecedores']
+            nome2,pantone_codigo2 = data[card+2]['nome'],data[card+2]['pantone_código']
+            red2,green2,blue2 = data[card+2]['red'],data[card+2]['green'],data[card+2]['blue']
+            c2,y2,m2,k2 = rgb_to_cmyk(data[card+2]['red'],data[card+2]['green'],data[card+2]['blue'])
+        
+            with container:
+                st.button('anterior', key='anterior', on_click=subtract)
+                script = ("<div style='display: flex; flex-direction: row; justify-content: space-around; margin: 0px; padding:0px;width: 700px ;margin: 0px auto; height: 450px;'><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'><div id='container' style='background-color: {}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p><p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f} <br>magenta: {:.2f} <br>key:{:.2f} </p> </div><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'> <div id='container' style='background-color: {}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p> <p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f} <br>magenta: {:.2f} <br>key:{:.2f} </p> </div><div style='background-color: white ; width: 220px; height: 400px;border-radius: 10px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25); margin: 5px;'> <div id='container' style='background-color: {}; width: 200px; height: 200px; '></div> <p style='color:black; margin: 0px; padding:0px'>{}: {}</p> <p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p> <p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p> <p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f}<br>magenta: {:.2f}<br>key:{:.2f} </p> </div></div>").format(hexadecimal,fornecedores,nome,pantone_codigo,red,green,blue,float(c),float(m),float(y),float(k),hexadecimal1,fornecedores1,nome1,pantone_codigo1,red1,green1,blue1,float(c1),float(m1),float(y1),float(k1),hexadecimal2,fornecedores2,nome2,pantone_codigo2,red2,green2,blue2,float(c2),float(m2),float(y2),float(k2))
+                st.markdown(script, unsafe_allow_html=True)
+                st.button('proximo', key='proximo', on_click=add)
  
 st.title('Find me')
 st.subheader('Onde você acha sua cor',divider='rainbow')
