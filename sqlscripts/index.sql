a295e677ac55c4b11cca51bba88a188b8cecd705
Igor <igorberaldomo@gmai.com>
2024-11-01 14:32:30 -0300

    update index

--- sqlscripts/index.sql
+++ sqlscripts/index.sql
@@ -4,6 +4,8 @@ CREATE INDEX idx_green1 ON suvinil (green);
 CREATE INDEX idx_blue1 ON suvinil (blue);
 CREATE INDEX idx_hex1 ON suvinil (hexadecimal);
 CREATE INDEX idx_pantone_cod1 ON suvinil (pantone_código);
+CREATE INDEX idx_pantone_name1 ON suvinil (pantone_name);
+CREATE INDEX idx_pantone_hex1 ON suvinil (pantone_hex);
 CREATE INDEX idx_fornecedores1 ON suvinil (fornecedores);
 
 
@@ -13,4 +15,6 @@ CREATE INDEX idx_green2 ON coral (green);
 CREATE INDEX idx_blue2 ON coral (blue);
 CREATE INDEX idx_hex2 ON coral (hexadecimal);
 CREATE INDEX idx_pantone_cod2 ON coral (pantone_código);
+CREATE INDEX idx_pantone_name2 ON coral (pantone_name);
+CREATE INDEX idx_pantone_hex2 ON coral (pantone_hex);
 CREATE INDEX idx_fornecedores2 ON coral (fornecedores);
