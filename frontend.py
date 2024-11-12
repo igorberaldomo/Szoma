f76f960e39b76e9e6565f21c4f1dd776648a5d03
Igor <igorberaldomo@gmai.com>
2024-11-12 16:30:08 -0300

    no requirements

--- frontend.py
+++ frontend.py
@@ -1,94 +1,148 @@
-import colorthief
 import streamlit as st
-from streamlit_extras.stylable_container import stylable_container
-import numpy as np
 import pandas as pd
-import requests
-import json
+import numpy as np
 import time
+from colorthief import ColorThief
 from utils.rgb_to_cmyk import rgb_to_cmyk
+from utils.select_complementos import select_complementos
+from utils.search.search_hexadecimal import select_hexadecimal
+from utils.search.search_codigos import select_códigos
+from utils.search.search_id import select_id
+from utils.search.search_name_for_id import search_name_for_id
+from utils.search.primary_select import primary_select
+from utils.conect_to_engine_developer import conect_to_engine_developer
 
-if "count" not in st.session_state:
-    st.session_state.count = '0'
-                
+# Conectar ao banco de dados
+engine = conect_to_engine_developer()
+
+# Inicializar o estado da sessão
+if "resultados" not in st.session_state:
+    st.session_state.resultados = []
+if "complementos" not in st.session_state:
+    st.session_state.complementos = []
+
+# Estilização personalizada
 st.markdown("""
 <style>
 .st-emotion-cache-yfhhig.ef3psqc5{
 visibility:hidden
 }
-
 </style>
 """, unsafe_allow_html=True)
-query = True
 
+# Funções para processar as entradas do usuário
 def findrgb():
-    if  procura is not None or upload is not None:
+    if procura or upload:
         if upload is not None:
-            st.session_state.cliked = True
-            ct = colorthief.ColorThief(upload)
+            ct = ColorThief(upload)
             cor = ct.get_color(quality=1)
-            json_procura = {'cor': cor,'fornecedores':opcao_fornecedores}
-            response = requests.post("https://findme-backend.streamlit.app:5555/colors/",json=json_procura)
+            red, green, blue = cor
+            fornecedores = opcao_fornecedores
+            response_df = primary_select(red, green, blue, fornecedores)
+            st.session_state.resultados = response_df.to_dict(orient='records')
         elif procura != '':
+            fornecedores = opcao_fornecedores
             if procura[0].isalpha():
-                st.session_state.cliked = True
-                nome = {"nome":procura, "fornecedores":opcao_fornecedores}
-                response = requests.post("https://findme-backend.streamlit.app:5555/names/",json=nome)
+                nome = procura
+                request_id = search_name_for_id(nome)
+                response_df = select_id(request_id, nome, fornecedores)
+                st.session_state.resultados = response_df.to_dict(orient='records')
             elif procura[0].isnumeric():
-                codigo = {"codigo":procura, "fornecedores":opcao_fornecedores}
-                response = requests.post("https://findme-backend.streamlit.app:5555/codigos/",json=codigo)
+                codigo = procura
+                response_df = select_códigos(codigo, fornecedores)
+                st.session_state.resultados = response_df.to_dict(orient='records')
             elif procura[0] == '#':
-                hexa = {"hexadecimal":procura, "fornecedores":opcao_fornecedores}
-                response = requests.post("https://findme-backend.streamlit.app:5555/hex/",json=hexa)
-            return response
+                hexadecimal = procura
+                response_df = select_hexadecimal(hexadecimal, fornecedores)
+                st.session_state.resultados = response_df.to_dict(orient='records')
     else:
-        st.text('Por favor coloque uma imagem para verificar a cor')
+        st.text('Por favor, insira uma imagem ou um valor para procurar a cor')
 
-     
 def receivecolors():
-    response = requests.get("https://findme-backend.streamlit.app:5555/colors/",headers={'Content-Type': 'application/json',"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0'})
-    data = response.json()
-    cores_df = pd.DataFrame(data).filter(['id','nome','red','green','blue','hexadecimal','pantone_código','pantone_name','pantone_hex','fornecedores']) 
-    container = st.container()
-    st.toast('loading...')
-    time.sleep(1.5)
-    try:
+    if st.session_state.resultados:
+        data = st.session_state.resultados
+        cores_df = pd.DataFrame(data)
+        container = st.container()
+        st.toast('Carregando...')
+        time.sleep(1.5)
+        try:
+            # Processar a cor principal
+            cor_principal = data[0]
+            red, green, blue = cor_principal['red'], cor_principal['green'], cor_principal['blue']
+            c, m, y, k = rgb_to_cmyk(red, green, blue)
+            fornecedores = cor_principal['fornecedores']
+            nome = cor_principal['nome']
+            hexadecimal = cor_principal['hexadecimal']
+            pantone_codigo = cor_principal['pantone_código']
 
-        hexadecimal,fornecedores = (data[0]['hexadecimal']), data[0]['fornecedores']
-        nome,pantone_codigo = data[0]['nome'],data[0] ['pantone_código']
-        red,green,blue = data[0]['red'],data[0]['green'],data[0]['blue']
-        c,y,m,k = rgb_to_cmyk(data[0]['red'],data[0]['green'],data[0]['blue'])
+            # Calcular complementos
+            complementos_df = select_complementos(red, green, blue, tipo_de_palheta, opcao_fornecedores)
+            complementos = complementos_df.to_dict(orient='records')
+            st.session_state.complementos = complementos
 
-        response_complementos = requests.post("https://findme-backend.streamlit.app:5555/complementos/",json={'red': red, 'green': green, 'blue': blue,"palheta":tipo_de_palheta,'fornecedores':opcao_fornecedores}) 
-        complementos = requests.get("https://findme-backend.streamlit.app:5555/complementos/",headers={'Content-Type': 'application/json'})
-        complementos = complementos.json()
-        hexadecimalc1,fornecedoresc1 = (complementos[0]['hexadecimal']), complementos[0]['fornecedores']
-        nomec1,pantone_codigoc1 = complementos[0]['nome'],complementos[0]['pantone_código']
-        redc1,greenc1,bluec1 =complementos[0]['red'],complementos[0]['green'],complementos[0]['blue']
-        cc1,yc1,mc1,kc1 = rgb_to_cmyk(complementos[0]['red'],complementos[0]['green'],complementos[0]['blue'])
+            # Processar complementos
+            complemento1 = complementos[0]
+            redc1, greenc1, bluec1 = complemento1['red'], complemento1['green'], complemento1['blue']
+            cc1, mc1, yc1, kc1 = rgb_to_cmyk(redc1, greenc1, bluec1)
+            fornecedoresc1 = complemento1['fornecedores']
+            nomec1 = complemento1['nome']
+            hexadecimalc1 = complemento1['hexadecimal']
+            pantone_codigoc1 = complemento1['pantone_código']
 
-        hexadecimalc2,fornecedoresc2 = (complementos[1]['hexadecimal']), complementos[1]['fornecedores']
-        nomec2,pantone_codigoc2 = complementos[1]['nome'],complementos[1]['pantone_código']
-        redc2,greenc2,bluec2 = complementos[1]['red'],complementos[1]['green'],complementos[1]['blue']
-        cc2,yc2,mc2,kc2 = rgb_to_cmyk(complementos[1]['red'],complementos[1]['green'],complementos[1]['blue'])
-            
-        
-        with container:
+            complemento2 = complementos[1]
+            redc2, greenc2, bluec2 = complemento2['red'], complemento2['green'], complemento2['blue']
+            cc2, mc2, yc2, kc2 = rgb_to_cmyk(redc2, greenc2, bluec2)
+            fornecedoresc2 = complemento2['fornecedores']
+            nomec2 = complemento2['nome']
+            hexadecimalc2 = complemento2['hexadecimal']
+            pantone_codigoc2 = complemento2['pantone_código']
 
-            script = ("<div style='display: flex; flex-direction: row; justify-content: space-around; margin: 0px; padding:0px;width: 700px ;margin: 0px auto; height: 450px;'><div style='background-color: white ; width: 660px; height: 420px;; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25);border-radius: 10px 0px 0px 10px; '><div><h5 style='margin: 0px; padding:0px; color:black;'><strong>Cor principal: </strong></h5><div id='container' style='background-color: {}; width: 200px; height: 200px; '></div><p style='color:black; margin: 0px; padding:0px'>{}: {}</p> <p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p><p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p><p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f}<br>yellow: {:.2f}<br>magenta: {:.2f}<br>key:{:.2f}</p></div></div><div style='background-color: white ; width: 660px; height: 420px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25);'><div><h5 style='color:black;margin: 0px; padding:0px'>Cor secundária 1: </h5><div id='container' style='background-color: {}; width: 200px; height: 200px; '></div><p style='color:black; margin: 0px; padding:0px'>{}: {}</p><p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p><p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p><p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f}<br>yellow: {:.2f}<br>magenta: {:.2f}<br>key:{:.2f}</p></div></div><div style='background-color: white ; width: 660px; height: 420px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25);border-radius: 0px 10px 10px 0px;'><div><h5 style='margin: 0px; padding:0px; color:black;'>Cor secundária 2: </h5><div id='container' style='background-color: {}; width: 200px; height: 200px; '></div><p style='color:black; margin: 0px; padding:0px'>{}: {}</p><p style='color:black;margin: 0px; padding:0px;'>pantone: {}</p><p style='color:black;margin: 0px; padding:0px'>rgb: {},{},{} </p><p style='color:black;margin: 0px; padding:0px'>cyan: {:.2f} <br>yellow: {:.2f}<br>magenta: {:.2f}<br>key:{:.2f}</p></div></div></div>").format(hexadecimal,fornecedores,nome,pantone_codigo,red,green,blue,float(c),float(m),float(y),float(k),hexadecimalc1,fornecedoresc1,nomec1,pantone_codigoc1,redc1,greenc1,bluec1,float(cc1),float(mc1),float(yc1),float(kc1),hexadecimalc2,fornecedoresc2,nomec2,pantone_codigoc2,redc2,greenc2,bluec2,float(cc2),float(mc2),float(yc2),float(kc2))
-            st.markdown(script, unsafe_allow_html=True)
-    except:
+            with container:
+                script = ("<div style='display: flex; flex-direction: row; justify-content: space-around; margin: 0px; padding:0px;width: 700px ;margin: 0px auto; height: 450px;'>"
+                          "<div style='background-color: white ; width: 660px; height: 420px; padding: 10px;box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25);border-radius: 10px 0px 0px 10px;'>"
+                          "<div><h5 style='margin: 0px; padding:0px; color:black;'><strong>Cor principal:</strong></h5>"
+                          "<div id='container' style='background-color: {}; width: 200px; height: 200px;'></div>"
+                          "<p style='color:black; margin: 0px; padding:0px'>{}: {}</p>"
+                          "<p style='color:black;margin: 0px; padding:0px;'>Pantone: {}</p>"
+                          "<p style='color:black;margin: 0px; padding:0px'>RGB: {},{},{} </p>"
+                          "<p style='color:black;margin: 0px; padding:0px'>Cyan: {:.2f}<br>Magenta: {:.2f}<br>Yellow: {:.2f}<br>Key: {:.2f}</p>"
+                          "</div></div>"
+                          "<div style='background-color: white ; width: 660px; height: 420px; padding: 10px;"
+                          "box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25);'>"
+                          "<div><h5 style='color:black;margin: 0px; padding:0px'>Cor secundária 1:</h5>"
+                          "<div id='container' style='background-color: {}; width: 200px; height: 200px;'></div>"
+                          "<p style='color:black; margin: 0px; padding:0px'>{}: {}</p>"
+                          "<p style='color:black;margin: 0px; padding:0px;'>Pantone: {}</p>"
+                          "<p style='color:black;margin: 0px; padding:0px'>RGB: {},{},{} </p>"
+                          "<p style='color:black;margin: 0px; padding:0px'>Cyan: {:.2f}<br>Magenta: {:.2f}<br>Yellow: {:.2f}<br>Key: {:.2f}</p>"
+                          "</div></div>"
+                          "<div style='background-color: white ; width: 660px; height: 420px; padding: 10px;"
+                          "box-shadow: 2px 2px 2px 1.5px rgba(0, 0, 0, 0.25);border-radius: 0px 10px 10px 0px;'>"
+                          "<div><h5 style='margin: 0px; padding:0px; color:black;'>Cor secundária 2:</h5>"
+                          "<div id='container' style='background-color: {}; width: 200px; height: 200px;'></div>"
+                          "<p style='color:black; margin: 0px; padding:0px'>{}: {}</p>"
+                          "<p style='color:black;margin: 0px; padding:0px;'>Pantone: {}</p>"
+                          "<p style='color:black;margin: 0px; padding:0px'>RGB: {},{},{} </p>"
+                          "<p style='color:black;margin: 0px; padding:0px'>Cyan: {:.2f}<br>Magenta: {:.2f}<br>Yellow: {:.2f}<br>Key: {:.2f}</p>"
+                          "</div></div></div>").format(
+                    hexadecimal, fornecedores, nome, pantone_codigo, red, green, blue, c, m, y, k,
+                    hexadecimalc1, fornecedoresc1, nomec1, pantone_codigoc1, redc1, greenc1, bluec1, cc1, mc1, yc1, kc1,
+                    hexadecimalc2, fornecedoresc2, nomec2, pantone_codigoc2, redc2, greenc2, bluec2, cc2, mc2, yc2, kc2)
+                st.markdown(script, unsafe_allow_html=True)
+        except Exception as e:
+            st.write("Nenhuma cor encontrada")
+            st.write(f"Erro: {e}")
+    else:
         st.write("Nenhuma cor encontrada")
 
-st.title('Find me')
+# Interface do usuário
+st.title('Find Me')
 st.subheader('Onde você acha sua cor')
-upload = st.file_uploader('dê upload na imagem abaixo para verificar a cor', type=['png','jpg','jpeg'])
-opcao_fornecedores = st.selectbox('Em que categoria você quer procurar?', options=('todos', 'coral','suvinil'))
-tipo_de_palheta =st.selectbox('quais opções de palheta você está procurando?', options=('triade','complementar','análoga' ))
-procura = st.text_input('Digite o nome da cor, o código pantone(00-0000) ou o hexadecimal(#000000):')
+upload = st.file_uploader('Faça upload de uma imagem para verificar a cor', type=['png', 'jpg', 'jpeg'])
+opcao_fornecedores = st.selectbox('Em que categoria você quer procurar?', options=('todos', 'coral', 'suvinil'))
+tipo_de_palheta = st.selectbox('Quais opções de palheta você está procurando?', options=('triade', 'complementar', 'análoga'))
+procura = st.text_input('Digite o nome da cor, o código Pantone (00-0000) ou o hexadecimal (#000000):')
 
 button = st.button('Procurar', on_click=findrgb)
 
 receivecolors()
-
-
