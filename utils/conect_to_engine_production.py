6e7edec3dbcc6624e20dfcddecf065d9b5ebf881
Igor <igorberaldomo@gmai.com>
2024-11-12 16:41:53 -0300

    pull

--- utils/conect_to_engine_production.py
+++ utils/conect_to_engine_production.py
@@ -5,4 +5,4 @@ from sqlalchemy import create_engine
 def conect_to_engine_production():
     DATABASE_URL = st.secrets["AWS_URL"]
     engine = sqlalchemy.create_engine(DATABASE_URL, pool_size=5, max_overflow=10)
-    return engine
+    return engin
