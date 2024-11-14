36e1cac83b898797405e9f5d65176a1d99571253
Igor <igorberaldomo@gmai.com>
2024-11-14 16:19:43 -0300

    formatado o nome sherwin-willians

--- utils/search/search _for_nome.py
+++ utils/search/search _for_nome.py
@@ -3,11 +3,13 @@ from utils.conect_to_engine_production import conect_to_engine_production
 
 engine = conect_to_engine_production()
 def select_names(nome, fornecedores):
+    if fornecedores == 'sherwin-willians':
+        fornecedores = "sherwin_willians"
     seach_string = ""
     if fornecedores != "todos":
         search_string = f"SELECT * from {fornecedores} WHERE nome = '{nome}' or pantone_name = '{nome}' "
     else:
-        search_string = f"SELECT nome,red,green,blue,ncs,codigo_suvinil,hexadecimal,pantone_código,pantone_name,pantone_hex,fornecedores from suvinil WHERE nome = '{nome}' or pantone_name = '{nome}' union SELECT nome,red,green,blue,null as ncs,null as codigo_suvinil,hexadecimal,pantone_código,pantone_name,pantone_hex,fornecedores from coral WHERE nome = '{nome}' or pantone_name = '{nome}' "
+        search_string = f"SELECT nome,red,green,blue,ncs,codigo_suvinil,hexadecimal,pantone_código,pantone_name,pantone_hex,fornecedores from suvinil WHERE nome = '{nome}' or pantone_name = '{nome}' union SELECT nome,red,green,blue,null as ncs,null as codigo_suvinil,hexadecimal,pantone_código,pantone_name,pantone_hex,fornecedores from coral WHERE nome = '{nome}' or pantone_name = '{nome}' union SELECT nome,red,green,blue,null as ncs,null as codigo_suvinil,hexadecimal,pantone_código,pantone_name,pantone_hex,fornecedores from sherwin_willians WHERE nome = '{nome}' or pantone_name = '{nome}' "
 
     resultset = pd.read_sql(search_string, engine)
     return resultset
