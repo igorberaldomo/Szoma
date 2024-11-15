95a42f1099363d43203775543e7f3f0c6c111f16
Igor <igorberaldomo@gmai.com>
2024-11-15 12:31:12 -0300

    retirada do bug do nome

--- utils/search/search_name_for_id.py
+++ utils/search/search_name_for_id.py
@@ -5,13 +5,13 @@ def search_name_for_id(nome):
         nome  = f'"{nome}"'
         search_dict = json.load(file)
         select_id = 0
-        if nome in search_dict[0]["quickSearch"][0]:
+        if nome in search_dict["quickSearch"][0]:
             select_id = search_dict["quickSearch"][0][nome]
-        if nome in search_dict[0]["suvinil"][0]:
+        if nome in search_dict["suvinil"][0]:
             select_id = search_dict["suvinil"][0][nome]
-        if nome in search_dict[0]["coral"][0]:
+        if nome in search_dict["coral"][0]:
             select_id = search_dict["coral"][0][nome]
-        if nome in search_dict[0]["sherwin-willians"][0]:
+        if nome in search_dict["sherwin-willians"][0]:
             select_id = search_dict["sherwin-willians"][0][nome]
         print(select_id)
     return select_id
