81d02bd79130c18e18ddfe97aafa1aa23005f2a8
Igor <igorberaldomo@gmai.com>
2024-11-15 13:07:33 -0300

    retirada do bug do nome

--- utils/search/search_name_for_id.py
+++ utils/search/search_name_for_id.py
@@ -3,16 +3,21 @@ import json
 def search_name_for_id(nome):
     with open("search/search_dict.json", "r") as file:
         search_dict = json.load(file)
-        for keys in search_dict["quickSearch"][0]:
-            if nome in keys:
-                return search_dict["quickSearch"][0][""+nome+""]
-        for keys in search_dict["suvinil"][0]:
-            if nome in keys:
-                return search_dict["suvinil"][0][""+nome+""]
-        for keys in search_dict["sherwin-willians"][0]:
-            if nome in keys:
-                return search_dict["sherwin-willians"][0][""+nome+""]
-        for keys in search_dict["coral"][0]:
-            if nome in keys:
-                return search_dict["coral"][0][""+nome+""]
+        name_id = -1
+        if name_id == -1:
+            for keys in search_dict["quickSearch"][0]:
+                    if nome in keys:
+                        name_id = search_dict["quickSearch"][0][""+nome+""]
+        elif name_id == -1:
+            for keys in search_dict["suvinil"][0]:
+                    if nome in keys:
+                        name_id = search_dict["suvinil"][0][""+nome+""]
+        elif name_id == -1:
+            for keys in search_dict["sherwin-willians"][0]:
+                if nome in keys:
+                    name_id = search_dict["sherwin-willians"][0][""+nome+""]
+        elif name_id == -1:
+            for keys in search_dict["coral"][0]:
+                if nome in keys:
+                    name_id = search_dict["coral"][0][""+nome+""]
 
