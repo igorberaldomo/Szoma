5602b3fbc5288058f76f5d9ef767d938286aa1cd
Igor <igorberaldomo@gmai.com>
2024-11-19 14:07:56 -0300

    corrigindo primary select

--- utils/search/primary_select.py
+++ utils/search/primary_select.py
@@ -4,13 +4,16 @@ import pandas as pd
 
 engine = conect_to_engine_production()
 def primary_select(red, green, blue, tabela):
+    # distancia define o limite de busca para cima e para baixo
     distancia = 18
+    # min e max é os maiores e menores valores que um atributo pode ter
     maxred = red + distancia
     minred = red - distancia
     maxgreen = green + distancia
     mingreen = green - distancia
     maxblue = blue + distancia
     minblue = blue - distancia
+    # garanta que os valores estao entre 0 e 255 (limites do rgb)
     if maxred > 255:
         maxred = 255
     if minred < 0:
@@ -23,8 +26,25 @@ def primary_select(red, green, blue, tabela):
         maxblue = 255
     if minblue < 0:
         minblue = 0
+    # aplica a procura na tabela
     resultset = tabela[(tabela['red'] >= minred) & (tabela['red'] <= maxred) & (tabela['green'] >= mingreen) & (tabela['green'] <= maxgreen) & (tabela['blue'] >= minblue) & (tabela['blue'] <= maxblue)]
+    c = 0
+    menor_diferência = 0
+    posição = 0
+    # vai percorrer todos os resultados da procura e vai verificar qual tem a menor diferenca entre as cores pesquisadas e as cores da tabela
+    for c in resultset.index:  
+        diferença = abs(red - resultset['red'][c]) + abs(green - resultset['green'][c]) + abs(blue - resultset['blue'][c])
+        if c == 0:
+            menor_diferência = diferença
+            posição = c
+        if diferença < menor_diferência:
+            menor_diferência = diferença
+            posição = c
+        if menor_diferência == 0:
+            posição = c
+            break
+    # retorna o resultado ou vazio
     if resultset.empty:
         return []
     else:
-        return resultset
+        return resultset[posição]
