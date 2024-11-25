cb69bbc76ae3e1d3654a42ef2b2acf9a7f4187a5
Igor <igorberaldomo@gmai.com>
2024-11-25 12:33:31 -0300

    fixing if statement

--- utils/search/search_name_for_id.py
+++ utils/search/search_name_for_id.py
@@ -38,7 +38,7 @@ def search_name_for_id(nome, tabela):
                     name_id = search_dict["coral"][0][""+nome+""]
                     fornecedores = 'coral'
         # seleciona a tabela de acordo com o id encontrado
-        resultset = tabela[fornecedores]
-        st.write(name_id)
-        st.write(resultset)
+        for index, row in tabela[fornecedores].iterrows():
+            if index == name_id:
+                resultset = row
         return resultset
