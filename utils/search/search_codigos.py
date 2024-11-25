7a77154da28becb075ee96e36b6a3b66ec4193f6
Igor <igorberaldomo@gmai.com>
2024-11-25 14:34:09 -0300

    fixing select códigos

--- utils/search/search_codigos.py
+++ utils/search/search_codigos.py
@@ -8,10 +8,5 @@ engine = conect_to_engine_production()
 def select_códigos(codigo, tabela):
     for index, row in tabela.iterrows():
             if row['pantone_código'] == codigo:
-                resultset = row
-                break
-    return resultset
-
-# dct = {'nome': resultset[posição]['nome'], 'red': resultset[posição]['red'], 'green': resultset[posição]['green'], 'blue': resultset[posição]['blue'], 'ncs': resultset[posição]['ncs'], 'codigo_suvinil': resultset[posição]['codigo_suvinil'], 'hexadecimal': resultset[posição]['hexadecimal'], 'pantone_código': resultset[posição]['pantone_código'], 'pantone_name': resultset[posição]['pantone_name'], 'pantone_hex': resultset[posição]['pantone_hex'], 'fornecedores': resultset[posição]['fornecedores']} 
-                # dct = {k:[v] for k,v in dct.items()}     
-                # resultset_df = pd.DataFrame(dct)
+                resultset.append(row)
+    return resultset[0]
