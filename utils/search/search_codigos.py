a4ffd00867cb42671e55cdbd7bbc4d2718846a64
Igor <igorberaldomo@gmai.com>
2024-11-25 14:34:28 -0300

    fixing select códigos

--- utils/search/search_codigos.py
+++ utils/search/search_codigos.py
@@ -6,7 +6,9 @@ import streamlit as st
 
 engine = conect_to_engine_production()
 def select_códigos(codigo, tabela):
+    resultset = []
     for index, row in tabela.iterrows():
             if row['pantone_código'] == codigo:
                 resultset.append(row)
+        
     return resultset[0]
