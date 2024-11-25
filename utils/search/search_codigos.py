55613014fa7fd5b2077929c088d4fdc34a7be5aa
Igor <igorberaldomo@gmai.com>
2024-11-25 14:54:13 -0300

    fixing hexadecimal

--- utils/search/search_codigos.py
+++ utils/search/search_codigos.py
@@ -9,7 +9,6 @@ def select_códigos(codigo, tabela):
     resultset = dict()
     for index, row in tabela.iterrows():
         if row['pantone_código'] == codigo:
-            st.write(row)
             resultset = {'nome': row['nome'], 'red': row['red'], 'green': row['green'], 'blue': row['blue'], 'ncs': row['ncs'], 'codigo_suvinil': row['codigo_suvinil'], 'hexadecimal': row['hexadecimal'], 'pantone_código': row['pantone_código'], 'pantone_name': row['pantone_name'], 'pantone_hex': row['pantone_hex'], 'fornecedores': row['fornecedores']}
             break
     resultset = {k:[v] for k,v in resultset.items()}     
