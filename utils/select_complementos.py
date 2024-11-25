3db18b3e5ef86e072622e792a519a8345ee1bf65
Igor <igorberaldomo@gmai.com>
2024-11-25 15:39:45 -0300

    fixing complementos

--- utils/select_complementos.py
+++ utils/select_complementos.py
@@ -67,7 +67,7 @@ def select_complementos(red, green, blue, palheta, tabela):
         primeira_maior = ""
         segunda_maior = ""
         
-        st.write(tabela['red'])
+        st.write(tabela['red'] > menor_valor_de_menor and tabela['red'] < maior_valor_de_menor)
         # procura as cores que se enquadram na triade
         if maior == red:
             primeira_maior = "green"
