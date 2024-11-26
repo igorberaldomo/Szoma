dc616b07560ff9c6f0ea5bceed880fe296acf6d1
Igor <igorberaldomo@gmai.com>
2024-11-26 16:26:17 -0300

    fixing quick search

--- utils/search/search_name_for_id.py
+++ utils/search/search_name_for_id.py
@@ -15,28 +15,23 @@ def search_name_for_id(nome, tabela):
         if name_id == -1:
             for keys in search_dict["quickSearch"][0]:
                 if nome in keys:
-                    name_id = search_dict["quickSearch"][0][nome]
-                    tabela = tabela['coral'].to_json(orient="index")            
+                    name_id = search_dict["quickSearch"][0][nome]         
         if name_id == -1:
             for keys in search_dict["suvinil"][0]:
                 if nome in keys:
                     name_id = search_dict["suvinil"][0][nome]
-                    tabela = tabela['suvinil'].to_json(orient="index")
         if name_id == -1:
             for keys in search_dict["sherwin-willians"][0]:
                 if nome in keys:
                     name_id = search_dict["sherwin-willians"][0][nome]
-                    tabela = tabela['sherwin-willians'].to_json(orient="index")
         if name_id == -1:
             for keys in search_dict["anjo"][0]:
                 if nome in keys:
                     name_id = search_dict["anjo"][0][nome]
-                    tabela = tabela['anjo'].to_json(orient="index")
         if name_id == -1:
             for keys in search_dict["coral"][0]:
                 if nome in keys:
                     name_id = search_dict["coral"][0][nome]
-                    tabela = tabela['coral'].to_json(orient="index")
         # seleciona a tabela de acordo com o id encontrado
         st.write(tabela)
         c= 0
