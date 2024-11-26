f627635f92822365f69e1093bc57010d77bef8f8
Igor <igorberaldomo@gmai.com>
2024-11-26 15:33:05 -0300

    fixing quick search

--- utils/search/search_name_for_id.py
+++ utils/search/search_name_for_id.py
@@ -45,6 +45,6 @@ def search_name_for_id(nome, tabela):
             if c == name_id:
                 resultset = tabela[c]
             c += 1
-        resultset = pd.DataFrame.from_dict(resultset)
+        resultset = pd.read_json(resultset)
         st.write(resultset)
         return resultset
