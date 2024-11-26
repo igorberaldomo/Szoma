e55493edfa4ebcda15023aeb9ceaac31e5a38342
Igor <igorberaldomo@gmai.com>
2024-11-26 14:52:28 -0300

    fixing quick search

--- utils/search/search_name_for_id.py
+++ utils/search/search_name_for_id.py
@@ -40,7 +40,7 @@ def search_name_for_id(nome, tabela):
                     fornecedores = 'coral'
         # seleciona a tabela de acordo com o id encontrado
         tabela = tabela[fornecedores].to_json(orient="index")
-        st.write(tabela)
+        st.write(tabela['0'])
         c = 0
         for c in range(len(tabela)):
             if tabela[c]['name'] == name_id:
