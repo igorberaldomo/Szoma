68ebe12096d5390bd0e5ec632fefd94f6591a32c
Igor <igorberaldomo@gmai.com>
2024-11-27 17:20:50 -0300

    trying other methods

--- frontend.py
+++ frontend.py
@@ -209,7 +209,7 @@ def receivecolors():
 st.title('Find Me')
 st.subheader('Onde você acha sua cor')
 upload = st.file_uploader('Faça upload de uma imagem para verificar a cor', type=['png', 'jpg', 'jpeg'])
-upload = cropped_pic(upload)
+upload = crop_image(upload)
 opcao_fornecedores = st.selectbox('Em que categoria você quer procurar?', options=('todos', 'coral', 'suvinil', 'sherwin-willians','anjo'))
 tipo_de_palheta = st.selectbox('Quais opções de palheta você está procurando?', options=('triade', 'complementar', 'análoga'))
 procura = st.text_input('Digite o nome da cor, o código Pantone (00-0000) ou o hexadecimal (#000000):')
