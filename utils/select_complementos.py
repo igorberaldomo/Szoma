7bc623527b4da5ac4844011af850005fd1d98e1f
Igor <igorberaldomo@gmai.com>
2024-11-28 15:35:28 -0300

    pequena mudança para evitar cores iguais

--- utils/select_complementos.py
+++ utils/select_complementos.py
@@ -530,18 +530,20 @@ def select_complementos(red, green, blue, palheta, tabela):
             distancia_atual = atual_red + atual_green + atual_blue
             if c == 0 or distancia_atual < distancia:
                 if distancia_atual != 0:
-                    if segunda_menor == "red":
-                        if atual_red < atual_blue & atual_red < atual_green:
-                            menor_distancia_2 = x
-                            distancia = distancia_atual
-                    if segunda_menor == "green":
-                        if atual_green < atual_blue & atual_green < atual_red:
-                            menor_distancia_2 = x
-                            distancia = distancia_atual
-                    if segunda_menor == "blue":
-                        if atual_blue < atual_green & atual_blue < atual_red:
-                            menor_distancia_2 = x
-                            distancia = distancia_atual
+                    # verifica se o resultado selecionado nao e igual ao primeiro
+                    if resultado2[x] != resultado1[menor_distancia_1]:
+                        if segunda_menor == "red":
+                            if atual_red < atual_blue & atual_red < atual_green:
+                                menor_distancia_2 = x
+                                distancia = distancia_atual
+                        if segunda_menor == "green":
+                            if atual_green < atual_blue & atual_green < atual_red:
+                                menor_distancia_2 = x
+                                distancia = distancia_atual
+                        if segunda_menor == "blue":
+                            if atual_blue < atual_green & atual_blue < atual_red:
+                                menor_distancia_2 = x
+                                distancia = distancia_atual
                 elif distancia_atual == 0:
                     menor_distancia_1 = c
                     distancia = distancia_atual
