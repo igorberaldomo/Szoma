01369b5659d27ba78868b789bbffff8d68f1eb3e
Igor <igorberaldomo@gmai.com>
2024-12-09 16:30:24 -0300

    v

--- frontend.py
+++ frontend.py
@@ -267,7 +267,7 @@ def show_similar_colors():
                     script = (
                         "<div style='height: 400px; width: 700px; background-color:green display:flex; flex-direction: row'>"
                             "<p style='padding: 10px '>Cor Principal</p>"
-                            "<div style='margin: 100px 0px 0px -80px;height: 100px; width: 150px; background-color: {}; '>"
+                            "<div style='margin: 10px 0px 0px 0px;height: 100px; width: 150px; background-color: {}; '>"
                                 "<p style='padding: 60px 0px 0px 10px; font-size: 16px; color: {};'>{}:{}</p>"
                             "</div>"
                             "<div style='height: 300px; width: 500px;  padding: 0px; margin:-140px 10px 0px 180px; display: flex; flex-direction: column;'>"
