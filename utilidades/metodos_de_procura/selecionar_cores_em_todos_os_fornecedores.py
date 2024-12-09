5b0e4f875c46f638e59727c54eb9cde9ff6fd22a
Igor <igorberaldomo@gmai.com>
2024-12-09 12:40:16 -0300

    importação corrigida

--- utilidades/metodos_de_procura/selecionar_cores_em_todos_os_fornecedores.py
+++ utilidades/metodos_de_procura/selecionar_cores_em_todos_os_fornecedores.py
@@ -29,13 +29,15 @@ def selecionar_cores_em_todos_os_fornecedores(red, green,blue,suvinil):
     c = 0
     for c in range(len(suvinil)):
         if suvinil[c]['red'] >= minimo_vermelho and suvinil[c]['red'] <= maximo_vermelho and suvinil[c]['green'] >= minimo_verde and suvinil[c]['green'] <= maximo_verde and suvinil[c]['blue'] >= minimo_azul and suvinil[c]['blue'] <= maximo_azul: 
+            st.write(suvinil[c])
             resultados.append(suvinil[c])
         c += 1
     # busca na tabela coral
     c = 0
     coral = tabela["coral"]
     for c in range(len(coral)):
-        if coral[c]['red'] >= minimo_vermelho and coral[c]['red'] <= maximo_vermelho and coral[c]['green'] >= minimo_verde and coral[c]['green'] <= maximo_verde and coral[c]['blue'] >= minimo_azul and coral[c]['blue'] <= maximo_azul: 
+        if coral[c]['red'] >= minimo_vermelho and coral[c]['red'] <= maximo_vermelho and coral[c]['green'] >= minimo_verde and coral[c]['green'] <= maximo_verde and coral[c]['blue'] >= minimo_azul and coral[c]['blue'] <= maximo_azul:
+            st.write(suvinil[c])
             resultados.append(coral[c])
         c += 1
 
@@ -44,6 +46,7 @@ def selecionar_cores_em_todos_os_fornecedores(red, green,blue,suvinil):
     anjo = tabela["anjo"]
     for c in range(len(anjo)):
         if anjo[c]['red'] >= minimo_vermelho and anjo[c]['red'] <= maximo_vermelho and anjo[c]['green'] >= minimo_verde and anjo[c]['green'] <= maximo_verde and anjo[c]['blue'] >= minimo_azul and anjo[c]['blue'] <= maximo_azul: 
+            st.write(suvinil[c])
             resultados.append(anjo[c])
         c += 1
     # busca na tabela sherwin-willians
@@ -51,9 +54,9 @@ def selecionar_cores_em_todos_os_fornecedores(red, green,blue,suvinil):
     sherwin_willians = tabela["sherwin-willians"]
     for c in range(len(anjo)):
         if sherwin_willians[c]['red'] >= minimo_vermelho and sherwin_willians[c]['red'] <= maximo_vermelho and sherwin_willians[c]['green'] >= minimo_verde and sherwin_willians[c]['green'] <= maximo_verde and sherwin_willians[c]['blue'] >= minimo_azul and sherwin_willians[c]['blue'] <= maximo_azul: 
+            st.write(suvinil[c])
             resultados.append(sherwin_willians[c])
         c += 1
-
     
     if len(resultados) == 0:
         tem_resultados = False
